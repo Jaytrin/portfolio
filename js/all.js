a2266a819eabb5cc2980c31e2506647f73e19db9
Jaytrin <jaytrin.apinchapong@gmail.com>
2019-04-20 19:00:42 -0700

    updated navbar highlight

--- js/all.js
+++ js/all.js
@@ -1153,3 +1153,13 @@ function split_height_init(){
             
     })(jQuery);
 }
+
+$('#greeting').text(getDay());
+
+function getDay(){
+    var date = new Date();
+    var currentDay = date.getDay();
+    var dateArray = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+    var greeting = 'Happy ' + dateArray[currentDay] + '!'
+    return greeting
+}
